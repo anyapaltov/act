65ae07ed1a1c4d5796331f65bbd3a30395b1493b
Usuario <Usuario@DESKTOP-605R33K>
2016-09-11 20:04:12 -0500

    act commit

---
+++ src/SimpleMath.java
@@ -0,0 +1,25 @@
+
+
+public class SimpleMath {
+    
+static public int add (int a, int b)    
+{
+return a + b;
+}
+
+static public int multiply (int a, int b)    
+{
+return a * b;
+}
+
+static public int subtract (int a, int b)    
+{
+return a - b;
+}
+
+static public int divide (int a, int b)
+{
+return a / b;    
+}
+
+}
